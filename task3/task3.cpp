210587dbcfd5dc415dd814a9762f37e308f4b016
Denis <120037570+TochilovDenis@users.noreply.github.com>
2023-12-20 16:25:08 +0300

    Initial commit

--- task3/task3.cpp
+++ task3/task3.cpp
@@ -1,1 +1,18 @@
-﻿
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "");
+    int number;
+    cout << "Введите число: ";
+    cin >> number;
+
+    if (number > 0)
+        cout << "Вы ввели положительное число" << endl;
+    else if (number < 0)
+        cout << "Вы ввели отрицательное число" << endl;
+    else
+        cout << "Вы ввели ноль" << endl;
+
+    return 0;
+}
